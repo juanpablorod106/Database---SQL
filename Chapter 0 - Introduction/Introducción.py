b6a4582ae8070e2c62be1992bfdc8e8d3494ca21
JUANCHOROIS <juanpablorod106@gmail.com>
2024-07-25 10:36:01 -0400

    Chapter 0 - Introduction
    Chapter 0 - Notes and Topic Planning

---
+++ Chapter 0 - Introduction/Introducción.py
@@ -0,0 +1,92 @@
+                                                                                    Introducción
+                                                                        #Términos de repaso - Capitulo 0
+² Sistema gestor de bases de datos (SGBD).
+² Aplicaciones de sistemas de bases de datos.
+² Sistemas de archivos.
+² Inconsistencia de datos.
+² Restricciones de consistencia.
+² Vistas de datos.
+² Abstracción de datos.
+² Ejemplar de la base de datos.
+² Esquema.
+¤ Esquema de la base de datos.
+¤ Esquema físico.
+¤ Esquema lógico.
+² Independencia física de los datos.
+² Modelos de datos.
+¤ Modelo entidad-relación.
+¤ Modelo de datos relacional.
+¤ Modelo de datos orientado a objetos.
+¤ Modelo de datos relacional orientado a
+objetos.
+² Lenguajes de bases de datos.
+¤ Lenguaje de definición de datos.
+¤ Lenguaje de manipulación de datos.
+    DML Procedimental, Declarativo, No Procedimental.
+¤ Lenguaje de consultas.
+² Diccionario de datos.
+    Restricciones de dominio, autorizacion de lectura, de insercion (INSERT), de actualizacion, de eliminar.
+² Metadatos.
+² Transacciones.
+    Atomicidad, Consistencia, Durabilidad, Gestor de Recuperación, Recuperacion de fallos, Gestor de control de Concurrencia.
+² Concurrencia.
+² Programa de aplicación.
+² Diseño de base de Datos.
+    Diseño conceptual.
+    Normalizacion.
+    Especialización de Requerimientos funcionales.
+    Fase de Diseño Fisico.
+² Motor de base de Datos, gestor de almacenamiento.
+    Gestores de autorizacion e integridad.
+    Gestores de Transacciones.
+    Gestores de Archivos.
+    Bufer.
+    Archivos de Datos.
+    Diccionarios de Datos.
+    Indices.
+    Procesador de consultas.
+        Intérprete DDL.
+        Compilador DML.
+        Optimización de Consultas.
+        Motor de evaluación de consultas.
+² Administrador de bases de datos (ABD).
+² Arquitectura de Base de Datos.
+    Centralizado, Paralelo, Distribuido.
+² Arquitectura de Aplicación a Base de Datos.
+    Dos Niveles, Tres Niveles, Servidor de Aplicaciones.
+² Máquinas cliente y servidor.
+                                                                        #Ejercicios prácticos - Capitulo 0
+
+1.1 En este capítulo se han descrito varias ventajas importantes de los sistemas gestores de bases de
+datos. ¿Cuáles son sus dos inconvenientes?
+
+1.2 Indíquense siete lenguajes de programación que sean procedimentales y dos que no lo sean. ¿Qué
+grupo es más fácil de aprender a usar? Explíquese la respuesta.
+
+1.3 Indíquense seis pasos importantes que se deben dar para configurar una base de datos para una
+empresa dada.
+
+1.4 Considérese un array de enteros bidimensional de tamaño n £ m que se va a usar en el lenguaje de
+programación preferido del lector. Usando el array como ejemplo, ilústrese la diferencia (a) entre
+los tres niveles de abstracción de datos y (b) entre el esquema y los ejemplares.
+
+                                                                            #Ejercicios - Capitulo 0
+                                                                            
+1.5 Indíquense cuatro aplicaciones que se hayan usado que sea muy posible que utilicen un sistema de
+bases de datos para almacenar datos persistentes.
+
+1.6 Indíquense cuatro diferencias significativas entre un sistema de procesamiento de archivos y un
+SGBD.
+
+1.7 Explíquese la diferencia entre independencia de datos física y lógica.
+
+1.8 Indíquense cinco responsabilidades del sistema gestor de bases de datos. Para cada responsabilidad,
+explíquense los problemas que surgirían si no se asumiera esa responsabilidad.
+
+1.9 Indíquense al menos dos razones para que los sistemas de bases de datos soporten la manipulación
+de datos mediante un lenguaje de consultas declarativo como SQL, en vez de limitarse a ofrecer una
+biblioteca de funciones de C o de C++ para llevar a cabo la manipulación de los datos.
+
+1.10 Explíquense los problemas que causa el diseño de la tabla de la Figura 1.5.
+
+1.11 ¿Cuáles son las cinco funciones principales del administrador de bases de datos?
